c236de221ea06c8f301171b59e9f4690b31f25fe
layman127 <biock2105@gmail.com>
2025-01-08 00:33:33 +0100

    feat: add statistics for userProfile

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -11,7 +11,44 @@ export const getCurrentUserProfile = expressAsyncHandler(
             },
             select: { id: true, name: true, email: true },
         });
+
+        const countExerciceTimeComplete =
+            await prisma.exerciceLog.count({
+                where: {
+                    iserId: req.user.id,
+                    isCompleted: true,
+                },
+            });
+        const kgs = await prisma.exerciceTime.aggregate({
+            where: {
+                exerciceLog: { userId: req.user.id },
+                isCompleted: true,
+            },
+            _sum: { weight: true },
+        });
+        const workouts = await prisma.workoutLog.count({
+            where: { userId: user.id, isCompleted: true },
+        });
         res.status(200);
         res.json(user);
+        res.json({
+            ...user,
+            statistic: [
+                {
+                    label: 'Minutes',
+                    value: Math.ceil(
+                        countExerciceTimeComplete * 2.3 || 0
+                    ),
+                },
+                {
+                    label: 'Kilograms',
+                    value: kgs._sum.weight || 0,
+                },
+                {
+                    label: 'Workouts',
+                    value: workouts,
+                },
+            ],
+        });
     }
 );
